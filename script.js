ef9374e61ba7c89403f0d9736a5c86d76101d402
Craig Clayton <craig__clayton@outlook.com>
2024-12-17 13:13:46 +0000

    managed to get stop timer button to work

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ const body = document.querySelector('body');
 const h1 = document.querySelector('.heading');
 
 const startTimerButton = document.querySelector('.start');
+const stopTimerButton = document.querySelector('.stop');
 const timer = document.querySelector('.timer');
 
 setInterval(() => {
   // I borrowed this  "#" + Math.floor(Math.random() * 16777215).toString(16); from https://www.shecodes.io/athena/73117-how-to-generate-an-array-of-random-colors-in-javascript
   body.style.backgroundColor =
     '#' + Math.floor(Math.random() * 16777215).toString(16);
-}, 2000);
+}, 1000);
 
 setTimeout(() => {
   h1.classList.remove('hidden');
@@ -19,9 +20,14 @@ setTimeout(() => {
 }, 7000);
 
 let second = 0;
+let interval;
 startTimerButton.addEventListener('click', () => {
-  setInterval(() => {
+  interval = setInterval(() => {
     second++;
     timer.textContent = second;
   }, 1000);
+});
+
+stopTimerButton.addEventListener('click', () => {
+  clearInterval(interval);
 });
