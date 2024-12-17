6fb3c5a3ef402dc5b488b4767960af11fae44303
Craig Clayton <craig__clayton@outlook.com>
2024-12-17 12:01:07 +0000

    added a button to start a timer that is every second and displays it, I couldn't figure out how to stop it, I know clearInterval but something would of had to be global

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const body = document.querySelector('body');
 const h1 = document.querySelector('.heading');
 
+const startTimerButton = document.querySelector('.start');
+const timer = document.querySelector('.timer');
+
 setInterval(() => {
   // I borrowed this  "#" + Math.floor(Math.random() * 16777215).toString(16); from https://www.shecodes.io/athena/73117-how-to-generate-an-array-of-random-colors-in-javascript
   body.style.backgroundColor =
@@ -9,4 +12,16 @@ setInterval(() => {
 
 setTimeout(() => {
   h1.classList.remove('hidden');
-}, 5000);
+}, 2000);
+
+setTimeout(() => {
+  h1.classList.add('hidden');
+}, 7000);
+
+let second = 0;
+startTimerButton.addEventListener('click', () => {
+  setInterval(() => {
+    second++;
+    timer.textContent = second;
+  }, 1000);
+});
